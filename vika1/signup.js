9ff8bfa16e2dd965be6c87fe8f93a0ec1939b16b
Rúnar Vestmann <runarvestmann@gmail.com>
2021-06-07 02:36:45 +0000

    Added week 1 folder

---
+++ vika1/signup.js
@@ -0,0 +1,9 @@
+const firstName = document.getElementById('fname');
+const lastName = document.getElementById('lname');
+const email = document.getElementById('email');
+
+function signUp() {
+    console.log(firstName.value);
+    console.log(lastName.value);
+    console.log(email.value);
+}
